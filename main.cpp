5504c8154f2e36639fa26bb3946b9912858368d5
fanil724 <fanil724@gmail.com>
2023-01-18 21:06:52 +0300

    doing Sea_battle

--- main.cpp
+++ main.cpp
@@ -5,13 +5,27 @@ enum Hit_Status {
     Indefinitely,
     Hit = 1,
     Miss = -1,
-    Terminated = 0
+    Terminated = 3
 };
-
 struct Cell {
     Hit_Status hit;
     int Ship_ID;
 };
+const int move[4][2]{0, 1, 0, -1, 1, 0, -1, 0};
+
+void ComputerMove(Cell **&Player, size_t size, int x, int y) {
+    std::random_device rand;
+    int c;
+    std::uniform_int_distribution<int> disribution(1, 4);
+    c = disribution(rand);
+    if (Player[x+move[c][0]][y+move[c][1]].Ship_ID!=0) {
+        Player[x+move[c][0]][y+move[c][1]].hit=Hit;
+        ComputerMove(Player,size,x+move[c][0],y+move[c][1]);
+    }else{
+        Player[x+move[c][0]][y+move[c][1]].hit=Miss;
+        return;
+    }
+}
 
 bool ship(Cell **&Player, size_t size, int x, int y, int a, int b) {
     int i = 0, j = 0;
@@ -50,63 +64,135 @@ void random_placement_of_ships(Cell **&Player, size_t size) {
     std::random_device rand;
     int ship_size = 4, count_of_ships = 1;
     for (int j = ship_size; j > 0; j--) {
-        int count = 0;
-        for (int i = count_of_ships; i < 5; i++) {
-            std::uniform_int_distribution<int> disribution(0, 9);
-            int x = disribution(rand);
-            int y = disribution(rand);
-            std::uniform_int_distribution<int> disr(1, 4);
-            int direction = disr(rand);
-            std::cout << x << " " << y << " " << direction << std::endl;
-            switch (direction) {
-                case 1:
-                    if (ship(Player, size, x, y, x + ship_size, y) && ((x + ship_size) < size)) {
-                        for (int c = x; c < (x + ship_size); c++) {
-                            Player[c][y].Ship_ID = ship_size;
-                        }
-                        std::cout << "1\n";
-                    } else { i--; }
-                    break;
-                case 2:
-                    if (ship(Player, size, x, y, x - ship_size, y) && ((x - ship_size) > -1)) {
-                        for (int c = x - ship_size; c < x; c++) {
-                            Player[c][y].Ship_ID = ship_size;
-                        }
-                        std::cout << "2\n";
-                    } else { i--; }
-                    break;
-                case 3:
-                    if (ship(Player, size, x, y, x, y - ship_size) && ((y - ship_size) > -1)) {
-                        for (int c = y - ship_size; c < y; c++) {
-                            Player[x][c].Ship_ID = ship_size;
-                        }
-                        std::cout << "3\n";
-                    } else { i--; }
-                    break;
-                case 4:
-                    if (ship(Player, size, x, y, x, y + ship_size) && ((y + ship_size) < size)) {
-                        for (int c = y; c < (y + ship_size); c++) {
-                            Player[x][c].Ship_ID = ship_size;
-                        }
-                        std::cout << "4\n";
-                    } else { i--; }
-                    break;
+        for (int i = 0; i < count_of_ships; i++) {
+            int x, y;
+            do {
+                std::uniform_int_distribution<int> disribution(0, 9);
+                x = disribution(rand);
+                y = disribution(rand);
+            } while (Player[x][y].Ship_ID != 0);
+            if (ship(Player, size, x, y, x + ship_size, y) && ((x + ship_size) < size)) {
+                for (int c = x; c < (x + ship_size); c++) {
+                    Player[c][y].Ship_ID = ship_size;
+                }
+            } else if (ship(Player, size, x, y, x, y + ship_size) && ((y + ship_size) < size)) {
+                for (int c = y; c < (y + ship_size); c++) {
+                    Player[x][c].Ship_ID = ship_size;
+                }
+            } else if (ship(Player, size, x, y, x, y - ship_size) && ((y - ship_size) > -1)) {
+                for (int c = y - ship_size; c < y; c++) {
+                    Player[x][c].Ship_ID = ship_size;
+                }
+            } else if (ship(Player, size, x, y, x - ship_size, y) && ((x - ship_size) > -1)) {
+                for (int c = x - ship_size; c < x; c++) {
+                    Player[c][y].Ship_ID = ship_size;
+                }
+            } else { i--; }
+        }
+        count_of_ships++;
+        ship_size--;
+    }
+}
+
+void Player_Turn(Cell **&Player, size_t size) {
+    int x, y;
+    std::cout << "Select the cell you want to hit:";
+    do {
+        std::cin >> x >> y;
+        if (Player[x][y].hit != Indefinitely) {
+            std::cout << "You have already hit this cell, choose another:";
+        }
+    } while (Player[x][y].hit != Indefinitely);
+    if (Player[x][y].Ship_ID != 0) {
+        Player[x][y].hit = Hit;
+        Player_Turn(Player, size);
+    } else {
+        Player[x][y].hit = Miss;
+        return;
+    }
+}
+
+void Computer_Shot(Cell **&Player, size_t size) {
+    std::random_device rand;
+    int x, y;
+    do {
+        std::uniform_int_distribution<int> disribution(0, 9);
+        x = disribution(rand);
+        y = disribution(rand);
+    } while (Player[x][y].Ship_ID != 0);
+    if (Player[x][y].Ship_ID != 0) {
+        Player[x][y].hit = Hit;
+        ComputerMove(Player,size,x,y);
+    } else {
+        Player[x][y].hit = Miss;
+    }
+}
+
+void Print_Sea_Battlw_for_PC(Cell **&Player, size_t size) {
+    std::cout << "      Computer field!\n";
+    std::cout << "    ";
+    for (int i = 0; i < size; i++) {
+        std::cout << i + 1 << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "   ";
+    for (int i = 0; i < 22; i++) {
+        if (i == 0) {
+            std::cout << char(218);
+        } else if (i == 21) {
+            std::cout << char(191);
+        } else {
+            std::cout << char(196);
+        }
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < size; i++) {
+        if (i != size - 1) { std::cout << " "; }
+        std::cout << i + 1 << " " << char(179);
+        for (int j = 0; j < size; j++) {
+            if (Player[i][j].hit == Indefinitely) {
+                std::cout << "  ";
+            } else {
+                std::cout << Player[i][j].hit << " ";
             }
         }
-    }
-}
-
-void Print_Sea_Battlw_for_PC(Cell **Player, size_t size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            std::cout << Player[i][j].hit << " ";
-        }
+        std::cout << char(179);
         std::cout << std::endl;
     }
+    std::cout << "   ";
+    for (int i = 0; i < 22; i++) {
+        if (i == 0) {
+            std::cout << char(192);
+        } else if (i == 21) {
+            std::cout << char(217);
+        } else {
+            std::cout << char(196);
+        }
+    }
+    std::cout << std::endl;
 }
 
-void Print_Sea_Battlw_for_Player(Cell **Player, size_t size) {
+void Print_Sea_Battlw_for_Player(Cell **&Player, size_t size) {
+    std::cout << "      Player field!\n";
+    std::cout << "    ";
     for (int i = 0; i < size; i++) {
+        std::cout << i + 1 << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "   ";
+    for (int i = 0; i < 22; i++) {
+        if (i == 0) {
+            std::cout << char(218);
+        } else if (i == 21) {
+            std::cout << char(191);
+        } else {
+            std::cout << char(196);
+        }
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < size; i++) {
+        if (i != size - 1) { std::cout << " "; }
+        std::cout << i + 1 << " " << char(179);
         for (int j = 0; j < size; j++) {
             if (Player[i][j].Ship_ID != 0) {
                 std::cout << Player[i][j].Ship_ID << " ";
@@ -114,10 +200,21 @@ void Print_Sea_Battlw_for_Player(Cell **Player, size_t size) {
                 std::cout << "  ";
             }
         }
+        std::cout << char(179);
         std::cout << std::endl;
     }
+    std::cout << "   ";
+    for (int i = 0; i < 22; i++) {
+        if (i == 0) {
+            std::cout << char(192);
+        } else if (i == 21) {
+            std::cout << char(217);
+        } else {
+            std::cout << char(196);
+        }
+    }
+    std::cout << std::endl;
 }
-
 
 int main() {
     const size_t size = 10;
@@ -129,7 +226,9 @@ int main() {
     for (int i = 0; i < size; i++) {
         pc[i] = new Cell[size]{Indefinitely, 0};
     }
+    random_placement_of_ships(pc, size);
     random_placement_of_ships(player, size);
+    Print_Sea_Battlw_for_PC(pc, size);
     Print_Sea_Battlw_for_Player(player, size);
 }
 
